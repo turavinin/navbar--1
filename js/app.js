765ad4ae0b6e0f1d074a26f5dfd5cb7191af8d54
turavinin <aturavinin@gmail.com>
2020-12-21 14:13:07 -0300

    finish side nav animation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const hamBtn = document.querySelector('.hamburger');
 const hdrText = document.querySelector('.header__text');
+const header = document.querySelector('.header');
 
 hamBtn.addEventListener('click', () => {
   hdrText.classList.toggle('change');
+  header.classList.toggle('change');
 });
